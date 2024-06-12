d46dc3a999be453839a5e6c23a384240ae15be87
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-06-12 16:57:01 +0545

    added UI for displaying chats

--- client/app.js
+++ client/app.js
@@ -32,9 +32,26 @@ msgInput.addEventListener("keypress", () => {
 // Listen for messages
 socket.on("message", (data) => {
   activity.textContent = "";
+  const { name, text, time } = data;
   const li = document.createElement("li");
-  li.textContent = data;
-  document.querySelector("ul").appendChild(li);
+  li.className = "post";
+  if (name === nameInput.value) li.className = "post post--left";
+  if (name !== nameInput.value && name !== "Admin")
+    li.className = "post post--right";
+  if (name !== "Admin") {
+    li.innerHTML = `<div class="post__header ${
+      name === nameInput.value ? "post__header--user" : "post__header--reply"
+    }">
+      <span class="post__header--name">${name}</span> 
+      <span class="post__header--time">${time}</span> 
+      </div>
+      <div class="post__text">${text}</div>`;
+  } else {
+    li.innerHTML = `<div class="post__text">${text}</div>`;
+  }
+  document.querySelector(".chat-display").appendChild(li);
+
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
 });
 
 let activityTImer;
