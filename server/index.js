a9eec611d4012b2800a03b77a1a83260343b16d7
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-06-12 12:47:23 +0545

    basic websocket connection setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const ws = require("ws");
+const server = new ws.Server({ port: "3000" });
+
+server.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    //converting message of type buffer to string
+    const b = Buffer.from(message);
+    console.log(b.toString());
+    socket.send(`${message}`);
+  });
+});
